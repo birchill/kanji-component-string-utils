a012b57af6a6322745e8695c6d0d682d8e13fca8
Brian Birtles <birtles@gmail.com>
2025-04-01 16:26:55 +0900

    feat!: added parsing for radical indicators ("*")
    The component type is extended with an optional field, `is_rad`, which
    indicates a component is the radical for this character.
    
    In general the first component that matches the kanji radical is
    presumed to be the radical component. However, in some cases
    disambiguation is required such as 剪 where ⼑ is the radical, not ⺉.
    
    BREAKING CHANGE: The "*" character is now treated as a special character
    meaning "the preceding component is the radical for this character".
    
    BREAKING CHANGE: The `SubComponent` type is now called `BaseComponent`.

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,5 @@
-export type RootComponent = {
-  c: string;
-  var?: string;
-  sub?: Array<SubComponent>;
-};
-export type SubComponent = { c: string; var?: string };
+export type BaseComponent = { c: string; var?: string; is_rad?: true };
+export type RootComponent = BaseComponent & { sub?: Array<BaseComponent> };
 export type Components = Array<RootComponent>;
 
 export { parse } from './parse.js';
--- src/parse.ts
+++ src/parse.ts
@@ -1,25 +1,17 @@
-import type { Components, RootComponent, SubComponent } from './index.js';
+import type { Components, BaseComponent, RootComponent } from './index.js';
 
 export function parse(input: string): Components {
   const components: Components = [];
   let rest = input;
 
   while (rest.length > 0) {
-    const c = [...rest][0]!;
-    rest = rest.slice(c.length);
-
-    const component: RootComponent = { c };
-
-    if (rest[0] === '{') {
-      const { variant, rest: newRest } = parseVariant(rest);
-      component.var = variant;
-      rest = newRest;
-    }
+    let component: RootComponent;
+    ({ component, rest } = consumeComponent(rest));
 
     if (rest[0] === '[') {
-      const { subComponents, rest: newRest } = parseSubComponents(rest);
+      let subComponents: Array<BaseComponent>;
+      ({ subComponents, rest } = consumeSubComponents(rest));
       component.sub = subComponents;
-      rest = newRest;
     }
 
     components.push(component);
@@ -28,7 +20,33 @@ export function parse(input: string): Components {
   return components;
 }
 
-function parseVariant(input: string): { variant: string; rest: string } {
+function consumeComponent(input: string): {
+  component: BaseComponent;
+  rest: string;
+} {
+  const c = String.fromCodePoint(input.codePointAt(0)!);
+  if (c === '*') {
+    throw new Error('Invalid radical indicator');
+  }
+  let component: BaseComponent = { c };
+
+  let rest = input.slice(c.length);
+
+  if (rest[0] === '*') {
+    component.is_rad = true;
+    rest = rest.slice(1);
+  }
+
+  if (rest[0] === '{') {
+    let variant: string;
+    ({ variant, rest } = consumeVariant(rest));
+    component.var = variant;
+  }
+
+  return { component, rest };
+}
+
+function consumeVariant(input: string): { variant: string; rest: string } {
   const match = input.match(/^\{(\d{3}(?:-[a-z0-9]+)?)\}(.*)/s);
   if (!match) {
     throw new Error(`Invalid variant format: ${input}`);
@@ -36,11 +54,11 @@ function parseVariant(input: string): { variant: string; rest: string } {
   return { variant: match[1]!, rest: match[2]! };
 }
 
-function parseSubComponents(input: string): {
-  subComponents: SubComponent[];
+function consumeSubComponents(input: string): {
+  subComponents: BaseComponent[];
   rest: string;
 } {
-  const subComponents: SubComponent[] = [];
+  const subComponents: Array<BaseComponent> = [];
   let rest = input.slice(1); // Remove opening '['
 
   if (rest.length === 0) {
@@ -48,16 +66,9 @@ function parseSubComponents(input: string): {
   }
 
   while (rest[0] !== ']') {
-    const c = [...rest][0]!;
-    rest = rest.slice(c.length);
-
-    if (rest[0] === '{') {
-      const { variant, rest: newRest } = parseVariant(rest);
-      subComponents.push({ c, var: variant });
-      rest = newRest;
-    } else {
-      subComponents.push({ c });
-    }
+    let component: BaseComponent;
+    ({ component, rest } = consumeComponent(rest));
+    subComponents.push(component);
 
     if (rest.length === 0) {
       throw new Error('Unclosed subcomponent bracket');
